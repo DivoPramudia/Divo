48d7c1628e78ed3dd9ba155acb5ef18424cc453e
DivoPramudia <149493370+DivoPramudia@users.noreply.github.com>
2023-11-01 09:47:24 +0700

    Update tugas ddp C++.cpp

--- tugas ddp C++.cpp
+++ tugas ddp C++.cpp
@@ -3,13 +3,12 @@ using namespace std;
 
 int main() {
 	for (int i = 1; i <= 1; i++) {
-		cout << "----------" << endl;
+		cout << "-------" << endl;
 		
 		for (int j = 1; j <= 3; j++){
-			cout << "|__|__|__|";
+			cout << "|_|_|_|";
 			cout << endl;
 		}
 	}
 	return 0;
 }
-
